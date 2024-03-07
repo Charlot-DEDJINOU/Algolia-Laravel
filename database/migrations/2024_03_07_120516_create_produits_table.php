8374ff60daebb5c27e20c9a874a40ee7f51e3187
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-03-07 18:04:09 +0100

    Schema for my Model

---
+++ database/migrations/2024_03_07_120516_create_produits_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProduitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produits', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nom');
+            $table->text('description')->nullable();
+            $table->decimal('prix', 8, 2);
+            $table->integer('quantite');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produits');
+    }
+}
